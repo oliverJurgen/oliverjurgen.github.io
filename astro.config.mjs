7cc347e267505625276abba0342d17fefc1b701b
oliverJurgen <oliver.e@scopicsoftware.com>
2022-10-17 23:00:52 -0400

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://oliverjurgen.github.io",
-  base: "/",
 
   integrations: [mdx(), sitemap(), tailwind()],
   server: {
